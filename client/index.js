3d1088612ee88bd3919636d241409a9ec7ddb3de
Quan Yin Zhe <70968262+withyousoft1983@users.noreply.github.com>
2022-06-29 07:50:54 +0800

    Add load test client

---
+++ client/index.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const { io } = require("socket.io-client");
+const {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} = require("unique-names-generator");
+
+const URL = process.env.ENDPOINT || "http://localhost:3000";
+const MAX_CLIENTS = process.env.MAX_CLIENTS || 1000;
+const POLLING_PERCENTAGE = process.env.POLLING_PERCENTAGE || 0.05;
+const CLIENT_CREATION_INTERVAL_IN_MS =
+  process.env.CLIENT_CREATION_INTERVAL_IN_MS || 10;
+const EMIT_INTERVAL_IN_MS = process.env.EMIT_INTERVAL_IN_MS || 1000;
+
+let clientCount = 0;
+let lastReport = new Date().getTime();
+let packetsSinceLastReport = 0;
+
+const createClient = () => {
+  // for demonstration purposes, some clients stay stuck in HTTP long-polling
+  const transports =
+    Math.random() < POLLING_PERCENTAGE ? ["polling"] : ["polling", "websocket"];
+
+  const socket = io(URL, {
+    transports,
+  });
+
+
+
+  
+  setInterval(() => {
+    socket.emit("client to server event");
+  }, EMIT_INTERVAL_IN_MS);
+
+  socket.on("server to client event", () => {
+    packetsSinceLastReport++;
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`disconnect due to ${reason}`);
+  });
+
+  if (++clientCount < MAX_CLIENTS) {
+    setTimeout(createClient, CLIENT_CREATION_INTERVAL_IN_MS);
+  }
+};
+
+createClient();
+
+const printReport = () => {
+  const now = new Date().getTime();
+  const durationSinceLastReport = (now - lastReport) / 1000;
+  const packetsPerSeconds = (
+    packetsSinceLastReport / durationSinceLastReport
+  ).toFixed(2);
+
+  console.log(
+    `client count: ${clientCount} ; average packets received per second: ${packetsPerSeconds}`
+  );
+
+  packetsSinceLastReport = 0;
+  lastReport = now;
+};
+
+setInterval(printReport, 5000);
